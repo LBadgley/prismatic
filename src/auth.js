cc488a6c46a8d119d34ca3e9d61162cf32be305f
Laura Badgley <l.badgleyy@gmail.com>
2020-07-10 15:35:51 -0700

    removed login

--- src/auth.js
+++ src/auth.js
@@ -1,29 +1,29 @@
-import { auth, usersRef } from './firebase.js';
+// import { auth, usersRef } from './firebase.js';
 
-const options = { skipAuth : true };
+// const options = { skipAuth : true };
 
-const ui = new firebaseui.auth.AuthUI(auth);
+// const ui = new firebaseui.auth.AuthUI(auth);
 
-ui.start('#firebase-auth-container', {
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
+// ui.start('#firebase-auth-container', {
+//     signInOptions: [
+//         firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//         firebase.auth.GoogleAuthProvider.PROVIDER_ID
+//     ],
 
-    signInSuccessUrl: '/quiz.html',
+//     signInSuccessUrl: '/quiz.html',
 
-    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-    callbacks: {
-        signInSuccessWithAuthResult(authResult) {
-            const user = authResult.user;
-            usersRef.child(user.uid)
-                .set({
-                    uid: user.uid,
-                    displayName: user.displayName,
-                    photoURL: user.photoURL,
-                    quizColor: null
-                });
-            return true;
-        }
-    }
-});
+//     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+//     callbacks: {
+//         signInSuccessWithAuthResult(authResult) {
+//             const user = authResult.user;
+//             usersRef.child(user.uid)
+//                 .set({
+//                     uid: user.uid,
+//                     displayName: user.displayName,
+//                     photoURL: user.photoURL,
+//                     quizColor: null
+//                 });
+//             return true;
+//         }
+//     }
+// });
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-const config = {
-    apiKey: 'AIzaSyDatsoiOedmcTCIVyxaaaskencz27jycSs',
-    authDomain: 'prismatic-58ebe.firebaseapp.com',
-    databaseURL: 'https://prismatic-58ebe.firebaseio.com',
-    projectId: 'prismatic-58ebe',
-};
-export const app = firebase.initializeApp(config);
+// const config = {
+//     apiKey: 'AIzaSyDatsoiOedmcTCIVyxaaaskencz27jycSs',
+//     authDomain: 'prismatic-58ebe.firebaseapp.com',
+//     databaseURL: 'https://prismatic-58ebe.firebaseio.com',
+//     projectId: 'prismatic-58ebe',
+// };
+// export const app = firebase.initializeApp(config);
 
-export const auth = firebase.auth();
-const db = firebase.database();
-export const usersRef = db.ref('users');
-export const usersFavoriteColorSchemesRef = db.ref('users-favorite-color-schemes');
-export const usersFavoriteColoredPicturesRef = db.ref('users-favorite-colored-pictures');
+// export const auth = firebase.auth();
+// const db = firebase.database();
+// export const usersRef = db.ref('users');
+// export const usersFavoriteColorSchemesRef = db.ref('users-favorite-color-schemes');
+// export const usersFavoriteColoredPicturesRef = db.ref('users-favorite-colored-pictures');
